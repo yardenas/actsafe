4193cf74d29b01ff008ec20b88a391091b094395
Yarden <yarden.as@inf.ethz.ch>
2024-10-15 17:38:58 +0200

    Fix styling

--- docs/tailwind.config.mjs
+++ docs/tailwind.config.mjs
@@ -1,13 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('@tailwindcss/typography'),],
-	theme: {
-		fontFamily: {
-			'sans': ['Noto Sans']
-		}
-	}
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans"],
+      },
+      typography: ({ theme }) => ({
+        paper: {
+          css: {
+            "--tw-prose-body": theme("colors.slate.800"),
+            "--tw-prose-bullets": theme("colors.slate.800"),
+            "--tw-prose-pre-bg": theme("colors.pink.900"),
+            "--tw-prose-links": theme("colors.blue.500"),
+            a: {
+              color: theme("colors.blue.500"),
+              textDecoration: "none",
+              "&:hover": {
+                textDecoration: "underline",
+              },
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
